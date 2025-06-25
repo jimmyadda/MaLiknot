fce45221993de834e644f595011706cc8fd000d2
Jimmy Adda <ajimmy@technion.ac.il>
2025-06-25 23:14:46 +0300

    Use gunicorn to fix 502 on Railway

--- app.py
+++ app.py
@@ -4,9 +4,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    print("🚀 Flask app.py loaded")
-    return "✅ Minimal Flask app running"
-
-if __name__ == '__main__':
-    print("👀 Starting minimal app")
-    app.run(host='0.0.0.0', port=5000)
+    return "✅ Gunicorn is serving your Flask app!"
