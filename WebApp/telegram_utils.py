d1ffc1777f7dd4a9bf37cb109cf370a63f18a8a1
jimmy adda <addajimmy@gmail.com>
2025-07-22 11:06:45 +0300

    message completed

--- WebApp/telegram_utils.py
+++ WebApp/telegram_utils.py
@@ -15,6 +15,13 @@ def send_telegram_message(chat_id, message=None, key=None, lang=None, **kwargs):
     if not message and key:
         lang = lang or get_user_language(chat_id)
         message = get_message(key, lang, **kwargs)
+        print("send_telegram_message",key, lang,message)
+        if not message:
+            print(f"❌ No message found for key={key}, lang={lang}, chat_id={chat_id}")
+            return {"error": "no_message"}
+    if not chat_id:
+        print("❌ Missing chat_id")
+        return {"error": "missing_chat_id"}
 
     url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
     payload = {
